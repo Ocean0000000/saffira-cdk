52a340e83f6b87b1a23cb51d79fea6e3850b4b8b
Ocean0000000 <vantocean509@gmail.com>
2024-12-30 22:18:07 -0800

    Create s3 bucket to practice cdk commands

--- lib/saffira-cdk-stack.ts
+++ lib/saffira-cdk-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SaffiraCdkStack extends cdk.Stack {
@@ -12,5 +13,19 @@ export class SaffiraCdkStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'SaffiraCdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    // Create an S3 bucket
+    const bucket = new s3.Bucket(this, 'SaffiraAIStudentDataBucket', {
+      bucketName: `saffira-ai-student-data-${cdk.Aws.ACCOUNT_ID}`, // Optional: set a unique name globally
+      publicReadAccess: false, // Disable public access for security
+      versioned: true, // Enable versioning for auditability
+      encryption: s3.BucketEncryption.S3_MANAGED, // Enable encryption
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Automatically delete during stack cleanup (use cautiously in production)
+    });
+
+    // Output the bucket name
+    new cdk.CfnOutput(this, 'BucketName', {
+      value: bucket.bucketName,
+    });
   }
 }
